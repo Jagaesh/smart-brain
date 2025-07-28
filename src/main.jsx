1314440803e52ee72aeab0181b339d1db9a4858a
Jagaesh <charles.techjourney@gmail.com>
2025-07-28 22:39:05 +0200

    1st draft of front-end

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'tachyons'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+root = createRoot(document.getElementById('root'))
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
